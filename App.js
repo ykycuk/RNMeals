e8ea6bc1f178015b5e3a2b25d163630e529babfc
BatyrArtem <artembatyr88@gmail.com>
2022-08-31 18:03:14 +0300

    add styling to meal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack'
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,32 +1,81 @@
-import { View, Text, Image } from 'react-native'
-import MealDetails from '../components/MealDetails';
+import { useLayoutEffect } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
 
+import IconButton from '../components/IconButton';
+import List from '../components/MealDetail/List';
+import Subtitle from '../components/MealDetail/Subtitle'
+import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data'
 
-function MealDetailScreen({route}) {
+function MealDetailScreen({route, navigation}) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
+    function headerButtonPressHandler() {
+        console.log("Pressed!")
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <IconButton 
+                        icon="star"
+                        color="white"
+                        onPress={headerButtonPressHandler}
+                    />
+                )
+            }
+        })
+    }, [navigation, headerButtonPressHandler])
+
     return (
-        <View>
-            <Image source={{uri: selectedMeal.imageUrl}}/>
-            <Text>{selectedMeal.title}</Text>
+        <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{uri: selectedMeal.imageUrl}}/>
+            <Text style={styles.title}>{selectedMeal.title}</Text>
             <MealDetails 
                 duration={selectedMeal.duration}
                 complexity={selectedMeal.complexity}
                 affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
             />
-            <Text>Ingredients</Text>
-            {selectedMeal.ingredients.map((ingredient) => (
-                <Text key={ingredient}>{ingredient}</Text>
-            ))}
-            <Text>Steps</Text>
-            {selectedMeal.steps.map((step) => (
-                <Text key={step}>{step}</Text>
-            ))}
-        </View>
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients}/>
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps}/>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32
+    },
+    image: {
+        width: '100%',
+        height: 350
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white'
+    },
+    detailText: {
+        color: 'white'
+    },
+    listOuterContainer: {
+        alignItems: 'center'
+    },
+    listContainer: {
+        width: '80%'
+    }
+})
